410d174f29ed8ad199490a754e8e27e9ef8f6f57
bjorn <bjorn@bjorn-desktop>
2011-06-18 18:56:02 +0200

    send layout added

--- src/com/bitcoin/bitpay/BitPay.java
+++ src/com/bitcoin/bitpay/BitPay.java
@@ -4,6 +4,8 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TabHost;
 
 public class BitPay extends TabActivity {
@@ -50,5 +52,9 @@ public class BitPay extends TabActivity {
 		tabHost.addTab(spec);
 
 		tabHost.setCurrentTab(0);
+		
+		
 	}
+	
+	
 }
--- src/com/bitcoin/bitpay/SendActivity.java
+++ src/com/bitcoin/bitpay/SendActivity.java
@@ -2,14 +2,15 @@ package com.bitcoin.bitpay;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SendActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        TextView textview = new TextView(this);
-        textview.setText("This is the Send tab");
-        setContentView(textview);
+        setContentView(R.layout.send_layout);
+
+
     }
 }
