2843d4ae5f499dcd3306615b1910f95b0de07756
bjorn <bjorn@bjorn-desktop>
2011-06-18 23:55:40 +0200

    some layout changes, added onClick handlers.
    Change-Id: I9c63f163cb40a17365095d9a50721bb10a625d1c

--- src/com/bitcoin/bitpay/SendActivity.java
+++ src/com/bitcoin/bitpay/SendActivity.java
@@ -2,23 +2,49 @@ package com.bitcoin.bitpay;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
-public class SendActivity extends Activity {
+public class SendActivity extends Activity implements OnClickListener{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_layout);
 
         
-        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        Spinner spinner = (Spinner) findViewById(R.id.account_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.planets_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
 
 
         
         
     }
+
+	@Override
+	public void onClick(View arg0) {
+		if(R.id.button1 == arg0.getId())	{
+			Log.v(TAG, "onClick: button1");
+		}
+		else if(R.id.button2 == arg0.getId())	{
+			Log.v(TAG, "onClick: button2");
+		}
+			
+		
+	}
+	
+	private static final String TAG = "test";
+
 }
